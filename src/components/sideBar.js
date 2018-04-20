22fdc511ac08fde312b411839c4a2eb027972e87
Unknown <kemkes-tobias@web.de>
2018-04-20 17:01:20 +0200

    final design

--- src/components/sideBar.js
+++ src/components/sideBar.js
@@ -3,6 +3,9 @@ import '../assets/css/sideBar.css';
 import DataController from '../library/DataController';
 import History from './history';
 
+import logo from'../assets/logo-image.svg';
+
+
 class SideBar extends Component {
     constructor(props) {
         super(props);
@@ -21,9 +24,7 @@ class SideBar extends Component {
         } else {
             return (
                 <div>
-                    <div id="logo">
-                        <div id="logo-image"></div>
-                    </div>
+                    <img id="logo" src={logo}/>
                     <div id="nav-bar">
                         <div className="nav-bar-item" onClick={this.props.changeToHistory}>
                             History
